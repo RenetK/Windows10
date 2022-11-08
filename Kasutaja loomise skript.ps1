afe12a30c2d664909c01824b27bac0aa9069563c
Renet <renet.kuusk@gmail.com>
2022-11-08 02:41:03 -0800

    Kasutaja loomine

---
+++ Kasutaja loomise skript.ps1
@@ -0,0 +1,3 @@
+﻿$KasutajaParool - ConevertTo-SecureString "qwerty" -AsPlainText -Force
+
+New-LocalUser "Kasutaja1" -Password $KasutajaParool -FullName "Esimene Kasutaj" -Description "Local account - kasutaja1"
