e85d0e98f8962b8096e9c07ad36f02f2d372951e
Renet <renet.kuusk@voco.com>
2022-11-08 03:48:20 -0800

    lisasin parameetrid

--- Kasutaja loomise skript.ps1
+++ Kasutaja loomise skript.ps1
@@ -1,3 +1,10 @@
-﻿$KasutajaParool - ConevertTo-SecureString "qwerty" -AsPlainText -Force
+﻿#defineerime muutujad argumentide salvestamiseks
+param(
+$Kasutajanimi - $args[0]
+$Taisnimi - $args[1]
+$Kontokirjeldus - [2]
+)
+#loome paarooli
+$KasutajaParool - ConevertTo-SecureString "qwerty" -AsPlainText -Force
 
 New-LocalUser "Kasutaja1" -Password $KasutajaParool -FullName "Esimene Kasutaj" -Description "Local account - kasutaja1"
