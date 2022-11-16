296fa73efeaef293f266ed7f83a927491494701b
Renet <renet.kuusk@voco.ee>
2022-11-16 01:31:30 -0800

    Kasutaja loomine failist

---
+++ Kasutajaloomine_failist.ps1
@@ -0,0 +1,13 @@
+﻿#Faili asukoht
+$Fail = "C:\Users\Renet\Desktop\skriptimine\kasutajad.csv"
+
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+foreach ($kasutaja in $Kasutajad)
+{
+    $Kasutajanimi = $Kasutaja.Kasutajanimi
+    $Taisnimi = $kasutaja.Taisnimi
+    $Kontokirjeldus = $kasutaja.KontoKirjeldus
+    $Parool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+
+    New-LocalUser -Name $Kasutajanimi -Password $Parool -FullName $Taisnimi -Description $Kontokirjeldus
+}
