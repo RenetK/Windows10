68c0e47852d4219e1b3a081ff1dd970ad1c5a399
Renet <renet.kuusk@voco.ee>
2022-11-28 05:37:59 -0800

    Tegin ülesannde 5

--- Kasutajaloomine_ÜL1.ps1
+++ Kasutajaloomine_ÜL1.ps1
@@ -6,13 +6,16 @@ $Kasutajanimi = $Eesnimi + "." + $Perenimi
 $Taisnimi = $Eesnimi + " " + $Perenimi
 Write-Output "Kasutaja nimi on: $($Kasutajanimi.ToLower())"
 $Kontokirjeldus = Read-Host -Prompt "Sisesta konto kirjeldus" 
+#Loob parooli
+$Parool = [System.Web.Security.Membership]::GeneratePassword(8,2)
+Write-Output "Su parool on: $Parool"
 
-
+"$Kasutajanimi;$Parool" | Out-File -FilePath C:\Users\Renet\Desktop\skriptimine\kasutajanimi.txt -Append
 
 #loome paarooli
-$KasutajaParool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+$KasutajaParool = ConvertTo-SecureString "$Parool" -AsPlainText -Force
 #Teeme konto
 Try {
 New-LocalUser $Kasutajanimi.ToLower() -Password $KasutajaParool -FullName $Taisnimi -Description $Kontokirjeldus -ErrorAction Stop
 }
-Catch {Write-Host "See nimi ei sobi"}
+Catch {Write-Host "Midagi läks valesti"}
